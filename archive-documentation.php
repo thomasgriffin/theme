75fe8ddd9c6f5ec3e0d289c44ed3cdc9be2f0e6f
Andrew Munro <andrew@sumobi.com>
2014-09-09 20:43:25 +1200

    Documentation

--- archive-documentation.php
+++ archive-documentation.php
@@ -7,7 +7,6 @@ get_header(); ?>
 
 <?php affwp_page_header( 'Documentation' ); ?>
 
-
 <section class="section columns columns-3 grid">
 	<div class="wrapper">
 
@@ -33,15 +32,26 @@ get_header(); ?>
    				<p><?php echo $term->description; ?></p>
    				<?php endif; ?>
 
-   				<a href="<?php echo get_term_link( $term, $taxonomy ); ?>">View documentation &rarr;</a>
+
+          <?php
+            if ( $term->slug == 'easy-digital-downloads' ) {
+               $doc_url = 'https://easydigitaldownloads.com/documentation/';
+               $external = true;
+            } elseif( $term->slug == 'affiliatewp' ) {
+              $doc_url = 'http://affiliatewp.com/support/documentation/';
+               $external = true;
+            } else {
+              $doc_url = get_term_link( $term, $taxonomy );
+            }
+
+            $external = isset( $external ) == true ? ' target="_blank"' : '';
+         
+          ?>
+   				<a href="<?php echo $doc_url; ?>"<?php echo $external; ?>>View documentation &rarr;</a>
    			</article>	
    		<?php endforeach; ?>
 
 	</div>
 </section>
-
-
-
-
 
 <?php get_footer();
