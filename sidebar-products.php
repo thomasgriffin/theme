0c6107556a3ce4fab0cb3866ef4e13f76308a352
Andrew Munro <andrew@sumobi.com>
2014-10-21 12:42:50 +1300

    Add new list class to UL

--- sidebar-products.php
+++ sidebar-products.php
@@ -58,7 +58,7 @@
 		
 
 	
-		<ul>
+		<ul class="list">
 			<?php foreach( $terms as $term ) : 
 				// The $term is an object, so we don't need to specify the $taxonomy.
 				$term_link = get_term_link( $term );
