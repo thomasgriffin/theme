ba92e48e832b9bba7a6861e9a2e2660cad3f4a1a
Andrew Munro <andrew@sumobi.com>
2014-10-28 11:12:12 +1300

    EDD cart widget improvements

--- edd_templates/widget-cart-checkout.php
+++ edd_templates/widget-cart-checkout.php
@@ -1,7 +1,12 @@
 <li class="cart_item edd_subtotal">
-	<?php echo __( 'Subtotal:', 'edd' ). " <span class='subtotal'>" . edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?></span>
-</li>
+	
+	<span>
+		<?php echo __( 'Subtotal:', 'edd' ); ?>
 
-<li class="cart_item edd_checkout">
+		<span class="price">
+		<?php echo edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?>
+		</span>
+	</span>
+	
 	<a href="<?php echo edd_get_checkout_uri(); ?>" class="button"><?php _e( 'Checkout', 'edd' ); ?></a>
 </li>
