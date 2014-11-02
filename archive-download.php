a46bd8eaf4fa5e7fb868c48de4ad30b38500ea77
Andrew Munro <andrew@sumobi.com>
2014-11-02 18:48:47 +1300

    Product coming soon image

--- archive-download.php
+++ archive-download.php
@@ -22,47 +22,40 @@ get_header(); ?>
 		    
 			<?php if ( ! pp_product_is_coming_soon( get_the_ID() ) || current_user_can( 'manage_options' ) ) : ?>
 
-				
-	    	<?php pp_post_thumbnail( 'affwp-grid-thumbnail' ); ?>
-	    			<h2 class="entry-title">
-	    				<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">
-	    		    		<?php the_title(); ?>
-	    		    	</a>
-	    	    	</h2>
-				
-				
-		    <?php elseif ( pp_product_is_coming_soon( get_the_ID() ) ) : ?>
-		    	
-		    	<?php /*	  	
-	    		<h2><?php the_title(); ?></h2>
-	    		*/ ?>
-	    	
-	    		<div class="post-thumbnail">
-	    			<?php if ( current_user_can( 'manage_options' ) ) : ?>
-	    				<?php pp_post_thumbnail(); ?>
-	    			<?php else : ?>
-	    				<img alt="<?php the_title(); ?> - Coming Soon" src="<?php echo get_stylesheet_directory_uri() . '/images/add-ons-coming-soon.png'; ?>">
-	    			<?php endif; ?>	
-	    			
-	    		</div>
+	    		<?php pp_post_thumbnail( 'affwp-grid-thumbnail' ); ?>
 
-			<?php endif; ?>	
-
-	       	<?php 
-		 		the_excerpt();
-		 	?>
+    			<h2 class="entry-title">
+    				<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">
+    		    		<?php the_title(); ?>
+    		    	</a>
+    	    	</h2>
+				
+				<?php the_excerpt(); ?>
 
 		 		<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>" class="link">
  		    		Learn More  &rarr;
  		    	</a>
 
-		 	<?php /*
-		 			 	<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>" class="link">
-		 		    		View <?php the_title(); ?> &rarr;
-		 		    	</a>
-		 	*/ ?>
+		    <?php elseif ( pp_product_is_coming_soon( get_the_ID() ) ) : ?>
+		    	
+		    	<div class="post-thumbnail">
+		    		<?php if ( current_user_can( 'manage_options' ) ) : ?>
+		    		<?php pp_post_thumbnail( 'affwp-grid-thumbnail' ); ?>
+		    		<?php else : ?>
+		    			<img alt="<?php the_title(); ?> - Coming Soon" src="<?php echo get_stylesheet_directory_uri() . '/images/product-coming-soon.png'; ?>">
+		    		<?php endif; ?>	
+		    		
+		    	</div>
+		   		
+		   		<h2 class="entry-title">
+		   			<?php the_title(); ?>
+		       	</h2>
+	    		
+	    		<?php the_excerpt(); ?>
+		 		Coming Soon
+ 		    	
+			<?php endif; ?>	
 
-		 	
 		</article>
 
 	<?php endwhile; ?>
