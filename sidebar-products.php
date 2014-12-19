a435bc38a152a49162e9ffc528bf19ea421f7bc2
Andrew Munro <andrew@sumobi.com>
2014-12-19 19:15:48 +1300

    Show related posts on products Fixes
    Fixes #36

--- sidebar-products.php
+++ sidebar-products.php
@@ -28,10 +28,6 @@
 
 	<?php echo pp_product_info( 'right' ); ?>
 
-
-
-
-
 	<?php
 		$support_url = get_post_meta( get_the_ID(), '_pp_product_support_url', true ) ? get_post_meta( get_the_ID(), '_pp_product_support_url', true ) : site_url( 'plugin-support' );
 		$target     = get_post_meta( get_the_ID(), '_pp_product_support_url', true ) ? ' target="_blank"' : '';
@@ -47,5 +43,41 @@
 		
 	
 	</div>
+
+
+	<?php 
+	/**
+	 * Related Posts
+	 */
+		$connected = new WP_Query( array(
+		  'connected_type'  => 'downloads_to_posts',
+		  'connected_items' => get_queried_object(),
+		  'nopaging'        => true,
+		) );
+
+	if ( $connected->have_posts() ) : ?>
+		<aside class="box">
+
+		<?php 
+			$heading = $connected->post_count > 1 ? __( 'Related Posts', 'pp' ) : __( 'Related Post', 'pp' );
+		?>
+			<h2><?php echo $heading; ?></h2>
+		 
+			<ul class="linked list">
+		    <?php while ( $connected->have_posts() ) : $connected->the_post(); ?>  
+		    
+		    <li>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<?php the_title(); ?>
+				</a>
+		    </li>   
+		    <?php endwhile; wp_reset_postdata(); ?>
+		    </ul>
+	  
+
+	</aside>
+	<?php endif; ?>
+
+	
 
 </div>
