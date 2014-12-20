12984f8695f6b9246566b369a08b6a5f743ded7b
Andrew Munro <andrew@sumobi.com>
2014-12-20 20:37:01 +1300

    Add pagination to archive pages

--- archive.php
+++ archive.php
@@ -24,6 +24,9 @@ get_header(); ?>
 
 				endwhile;
 
+				// Previous/next page navigation.
+					pp_paging_nav();
+
 			else :
 				// If no content, include the "No posts found" template.
 				get_template_part( 'content', 'none' );
