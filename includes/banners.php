767e4cfc525871d09f6f9286aa7cfb69277adff1
Andrew Munro <andrew@sumobi.com>
2014-12-21 19:34:36 +1300

    Add banners

---
+++ includes/banners.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Banners
+ */
+
+/**
+ * AffiliateWP
+ */
+function pp_banner_affwp() {
+?>
+	<aside class="widget">
+		<a href="http://affiliatewp.com">
+			<img title="AffiliateWP" src="http://affiliatewp.com/wp-content/themes/affiliatewp-master/images/affiliate-banners/affiliatewp-300x250-1.png">
+		</a>
+	</aside>
+<?php
+}
+
+/**
+ * EDD
+ */
+function pp_banner_edd() {
+?>
+	<aside class="widget">
+		<a href="https://easydigitaldownloads.com" target="_blank">
+			<img title="Easy Digital Downloads" src="<?php echo get_stylesheet_directory_uri() . '/images/300x250-edd.png'; ?>" />
+		</a>
+	</aside>
+<?php
+}
+
