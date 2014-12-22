04bc266787d4942fd2880a8b52fd75b9b928e619
Pippin Williamson <pippin@pippinsplugins.com>
2014-12-22 17:05:08 -0600

    Formatting

--- content.php
+++ content.php
@@ -12,61 +12,32 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( array( 'box' ) ); ?>>
 
-	<?php //do_action( 'pp_content_single_start' ); ?>
+	<?php if ( is_single() ) : ?>
 
-
+		<?php pp_post_thumbnail( 'large' ); ?>
 		
+		<?php do_action( 'pp_content_single_start' ); ?>
 
-		<?php if ( is_single() ) : ?>
-
-			<?php pp_post_thumbnail( 'large' ); ?>
-			
-			<?php do_action( 'pp_content_single_start' ); ?>
-
-		<?php else : ?>
+	<?php else : ?>
 
 		<div class="entry-meta">
 			<?php pp_entry_meta(); ?>
-
-
-			
-
-
-
 		</div>
 		
 		<h2 class="entry-title">
 			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</h2>
 
-		
-		<?php endif; ?>
 
-		
-
-		<!-- <div class="entry-meta"> -->
-			<?php /*
-			<span class="comments">
-				<?php comments_popup_link( __( '0', 'affwp' ), __( '1', 'affwp' ), __( '%', 'affwp' ) ); ?>
-				<svg width="37px" height="32px">
-					<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svg-defs.svg#icon-comments'; ?>"></use>
-				</svg>
-			</span>
-			*/ ?>
-		
-			
-			<?php // edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-		<!-- </div> -->
-
-	<!-- </header> -->
+	<?php endif; ?>
 
 	<?php if ( is_single() ) : ?>
-	<div class="entry-content">
-		<?php
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
-			
-		?>
-	</div><!-- .entry-content -->
+		<div class="entry-content">
+			<?php
+				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
+				
+			?>
+		</div><!-- .entry-content -->
 	
 	<?php else : // blog listing or learning page ?>
 
@@ -91,30 +62,23 @@
 
 	<?php endif; ?>
 
-
-
-
-<?php
+	<?php
 	// Upsell non-members to premium content
-if ( has_category( 'member-restricted' ) && is_singular() && ! rcp_is_active() ) : ?>
+	if ( has_category( 'subscriber-only' ) && is_singular() && ! rcp_is_active() ) : ?>
 		<a href="<?php echo site_url('join-the-site'); ?>" class="upsell">
 		<div class="box columns columns-2">
 			
-				<div class="col">
-					<?php if ( has_category( 'subscriber-only' ) ) : ?>
-						<p>Join now to gain access to this tutorial and more.</p>
-					<?php elseif ( has_category( 'free-members' ) ) : ?>
-						<p>Join now to gain access to an ever-growing vault of information.</p>
-					<?php endif; ?>
-				</div>
+			<div class="col">
+				<p>Join now to gain access to this tutorial and more.</p>
+			</div>
 
-				<div class="col align-right">
-					<span class="button">Join Now</span>
-				</div>
+			<div class="col align-right">
+				<span class="button">Join Now</span>
+			</div>
 			
 		</div>
 		</a>
-<?php endif; ?>
+	<?php endif; ?>
 
 </article>
 
--- home.php
+++ home.php
@@ -6,21 +6,21 @@
 	<div class="columns-main-side columns">
 		<div class="wrapper">
 			<div class="primary col content-area">
-			<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+				<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
 
-			<?php endwhile; ?>
-			
-			<nav class="nav-links columns columns-2 clear">
-				<span class="nav-previous col">
-					<?php next_posts_link( __( '&larr; Older', 'affwp' ) ); ?>
-				</span>
+				<?php endwhile; ?>
+				
+				<nav class="nav-links columns columns-2 clear">
+					<span class="nav-previous col">
+						<?php next_posts_link( __( '&larr; Older', 'affwp' ) ); ?>
+					</span>
 
-				<span class="nav-next col">
-					<?php previous_posts_link( __( 'Newer &rarr;', 'affwp' ) ); ?>
-				</span>
-			</nav>
+					<span class="nav-next col">
+						<?php previous_posts_link( __( 'Newer &rarr;', 'affwp' ) ); ?>
+					</span>
+				</nav>
 
 			</div>
 		
