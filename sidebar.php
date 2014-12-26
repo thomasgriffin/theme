51af26d3f33d2b9b22b8e2232007f3d9ca2d88ae
Andrew Munro <andrew@sumobi.com>
2014-12-27 12:52:41 +1300

    Show EDD cart in sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,16 +4,42 @@
  *
  * @since 1.0
  */
+
+$affiliate_area_id = affiliate_wp()->settings->get( 'affiliates_page' );
 ?>
 
 <div class="primary-sidebar widget-area col right" role="complementary">
 	
-	<?php echo pp_single_post_type_info( 'right' ); ?>
+	<?php 
+	/**
+	 * Post information
+	 */
+	echo pp_single_post_type_info( 'right' ); 
+
+	?>
 
 	<?php 
-		$affiliate_area_id = affiliate_wp()->settings->get( 'affiliates_page' );
-	?>
+	/**
+	 * Show the shopping cart
+	 */
 	
+	$cart_items    = edd_get_cart_contents();
+	$cart_quantity = edd_get_cart_quantity();
+	$display       = $cart_quantity > 0 ? '' : 'style="display:none;"';
+	
+	if ( $cart_items ) : ?>
+		<aside class="">
+		<h2>Ready to purchase?</h2>
+		<ul class="edd-cart">
+			<?php foreach ( $cart_items as $key => $item ) : ?>
+				<?php echo edd_get_cart_item_template( $key, $item, false ); ?>
+			<?php endforeach; ?>
+
+			<?php edd_get_template_part( 'widget', 'cart-checkout' ); ?>
+		</ul>
+		</aside>
+	<?php endif; ?>
+
 	<?php 
 	/**
 	 * Show a button for non logged in users to join the site
