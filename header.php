8a5930c568342636ea8a9120dbd3e973a2abc03f
Andrew Munro <andrew@sumobi.com>
2015-11-27 11:22:53 +1300

    tweak notices

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	
+
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
@@ -31,11 +31,13 @@
 
 <body <?php body_class(); ?>>
 
+<?php do_action( 'pp_site_before' ); ?>
+
 <div id="site" class="hfeed">
 
-	
 
-	
+
+
 
 	<?php do_action( 'affwp_content_before' ); ?>
 	<div id="content">
