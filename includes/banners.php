ebc27d6a4e8530898949decb5fe4ca2fff7e0016
Andrew Munro <andrew@sumobi.com>
2016-01-29 21:42:36 +1300

    add utm links

--- includes/banners.php
+++ includes/banners.php
@@ -9,7 +9,7 @@
 function pp_banner_affwp() {
 ?>
 	<aside class="widget">
-		<a href="https://affiliatewp.com" target="_blank">
+		<a href="https://affiliatewp.com/?utm_campaign=affwp&utm_source=pippinsplugins&utm_medium=banner" target="_blank">
 			<img title="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/300x250-affwp.png'; ?>">
 		</a>
 	</aside>
@@ -22,7 +22,7 @@ function pp_banner_affwp() {
 function pp_banner_edd() {
 ?>
 	<aside class="widget">
-		<a href="https://easydigitaldownloads.com" target="_blank">
+		<a href="https://easydigitaldownloads.com/?utm_campaign=edd&utm_source=pippinsplugins&utm_medium=banner" target="_blank">
 			<img title="Easy Digital Downloads" src="<?php echo get_stylesheet_directory_uri() . '/images/300x250-edd.png'; ?>" />
 		</a>
 	</aside>
@@ -35,7 +35,7 @@ function pp_banner_edd() {
 function pp_banner_rcp() {
 ?>
 	<aside class="widget">
-		<a href="http://restrictcontentpro.com" target="_blank">
+		<a href="http://restrictcontentpro.com/?utm_campaign=rcp&utm_source=pippinsplugins&utm_medium=banner" target="_blank">
 			<img title="Restrict Content Pro" src="<?php echo get_stylesheet_directory_uri() . '/images/300x250-rcp.png'; ?>" />
 		</a>
 	</aside>
